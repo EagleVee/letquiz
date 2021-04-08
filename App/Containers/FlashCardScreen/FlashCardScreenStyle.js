dac4d693f7d3884c27e96031075aac0233ec316b
nguyenhuudat <cs.nguyenhuudat@gmail.com>
2021-04-08 12:29:37 +0700

    fix question UI, FlashCardScreen

---
+++ App/Containers/FlashCardScreen/FlashCardScreenStyle.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import { StyleSheet } from "react-native";
+import { WIDTH_RATIO } from 'Themes/Metrics';
+import { useThemeStyles } from 'Hooks/useThemeStyles';
+import { useThemeColors } from 'Hooks/useThemeColors';
+
+export const FlashCardScreenStyle = OriginalComponent => props => {
+  const ApplicationStyles = useThemeStyles();
+  const Colors = useThemeColors();
+  const styles = StyleSheet.create({
+    ...ApplicationStyles.screen,
+    ...ApplicationStyles.utils,
+    ...ApplicationStyles.text,
+  });
+
+  return (
+    <OriginalComponent
+      {...props}
+      styles={styles}
+    />
+  );
+}
