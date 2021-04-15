95d72021b4e4e9ce6a98130ca2904dd149802c3a
Đỗ Thành Đạt <kaitokid7a@gmail.com>
2021-04-15 11:24:22 +0700

    update HomeScreen

--- App/Containers/HomeScreen/HomeScreen.js
+++ App/Containers/HomeScreen/HomeScreen.js
@@ -1,22 +1,75 @@
 import React, { useContext, useEffect, useState } from "react";
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import { compose } from "ramda";
 import { useNavigationMethods } from "Hooks/useNavigationMethods";
 import { HomeScreenStyle } from "./HomeScreenStyle";
 import Container from "Components/Container/Container";
 import { RNScrollView } from "Components/RNComponents";
+import { useThemeColors } from "../../Hooks/useThemeColors";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import { WIDTH_RATIO } from "../../Themes/Metrics";
+import BlockDivider from "../../Components/Dividers/BlockDivider";
+import Feather from "react-native-vector-icons/Feather";
 
 function HomeScreen(props) {
   const { styles, navigation, route } = props;
   const NavigationMethods = useNavigationMethods();
+  const Colors = useThemeColors();
+
+  function onSearchPress() {
+    NavigationMethods.goToScreen("SearchScreen");
+  }
 
   return (
-    <Container>
+    <Container
+      statusBarColor={Colors.primary}
+      statusBarContent={"light-content"}>
       <RNScrollView>
-        <Text>HomeScreen</Text>
+        <View style={styles.header}>
+          <Text style={[styles.h4Bold, { color: Colors.white }]}>
+            Hello Duy!
+          </Text>
+          <Text style={[styles.h5Bold, { color: Colors.white }]}>
+            What do you want to do?
+          </Text>
+        </View>
+        <View style={styles.main}>
+          <TouchableOpacity style={styles.card} onPress={onSearchPress}>
+            <Ionicons
+              name={"search"}
+              size={36 * WIDTH_RATIO}
+              color={Colors.primaryButton}
+            />
+            <View style={styles.cardRight}>
+              <Text style={styles.h7BoldPrimary}>Find study resources</Text>
+              <BlockDivider height={8 * WIDTH_RATIO} />
+              <Text style={styles.h8Bold}>
+                Save time by finding pre-made study sets for your courses or
+                classes
+              </Text>
+            </View>
+          </TouchableOpacity>
+          <BlockDivider height={16 * WIDTH_RATIO} />
+          <TouchableOpacity style={styles.card} onPress={onSearchPress}>
+            <Feather
+              name={"plus-circle"}
+              size={36 * WIDTH_RATIO}
+              color={Colors.primaryButton}
+            />
+            <View style={styles.cardRight}>
+              <Text style={styles.h7BoldPrimary}>
+                Create your own study set
+              </Text>
+              <BlockDivider height={8 * WIDTH_RATIO} />
+              <Text style={styles.h8Bold}>
+                Create a study set that suits your lessons
+              </Text>
+            </View>
+          </TouchableOpacity>
+        </View>
       </RNScrollView>
     </Container>
   );
 }
 
-export default compose(HomeScreenStyle)(HomeScreen)
+export default compose(HomeScreenStyle)(HomeScreen);
--- App/Containers/HomeScreen/HomeScreenStyle.js
+++ App/Containers/HomeScreen/HomeScreenStyle.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import { StyleSheet } from "react-native";
-import { WIDTH_RATIO } from 'Themes/Metrics';
-import { useThemeStyles } from 'Hooks/useThemeStyles';
-import { useThemeColors } from 'Hooks/useThemeColors';
+import { WIDTH_RATIO } from "Themes/Metrics";
+import { useThemeStyles } from "Hooks/useThemeStyles";
+import { useThemeColors } from "Hooks/useThemeColors";
 
 export const HomeScreenStyle = OriginalComponent => props => {
   const ApplicationStyles = useThemeStyles();
@@ -11,12 +11,30 @@ export const HomeScreenStyle = OriginalComponent => props => {
     ...ApplicationStyles.screen,
     ...ApplicationStyles.utils,
     ...ApplicationStyles.text,
+    header: {
+      paddingTop: 30 * WIDTH_RATIO,
+      backgroundColor: Colors.primary,
+      alignItems: "center",
+      height: 250 * WIDTH_RATIO,
+      marginBottom: -60 * WIDTH_RATIO,
+    },
+    main: {
+      paddingHorizontal: 16 * WIDTH_RATIO,
+    },
+    card: {
+      paddingHorizontal: 16 * WIDTH_RATIO,
+      height: 120 * WIDTH_RATIO,
+      backgroundColor: Colors.cardBackground,
+      paddingTop: 12 * WIDTH_RATIO,
+      borderRadius: 8 * WIDTH_RATIO,
+      ...ApplicationStyles.utils.row,
+      ...ApplicationStyles.themedShadow,
+    },
+    cardRight: {
+      marginLeft: 16 * WIDTH_RATIO,
+      flex: 1,
+    },
   });
 
-  return (
-    <OriginalComponent
-      {...props}
-      styles={styles}
-    />
-  );
-}
+  return <OriginalComponent {...props} styles={styles} />;
+};
