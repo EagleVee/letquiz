1b10a52c33ff6426f4f4cf41698811247fcf26bf
Duy <dreamow9@gmail.com>
2021-04-22 09:43:52 +0700

    add study set redux

--- App/Redux/Reducers/ContentReducer.js
+++ App/Redux/Reducers/ContentReducer.js
@@ -2,16 +2,13 @@ import Immutable from "seamless-immutable";
 import { createReducer } from "reduxsauce";
 import { ContentTypes } from "Redux/Actions/ContentActions";
 import TransformHelper from "../../Utils/TransformHelper";
-import FAQTransform from "../../Transforms/FAQTransform";
 
 export const INITIAL_STATE = Immutable({
   faqs: [],
 });
 
 export const getFAQsSuccess = (state, action) => {
-  return state.merge({
-    faqs: TransformHelper.transformData(action.response.data, FAQTransform),
-  });
+  return state.merge({});
 };
 
 export const reducer = createReducer(INITIAL_STATE, {
