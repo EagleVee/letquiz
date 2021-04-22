477695a541b450de6cea16dd2c7a6ef73d455e0d
Đỗ Thành Đạt <kaitokid7a@gmail.com>
2021-04-22 14:08:28 +0700

    update test

--- __tests__/Integration/App.test.js
+++ __tests__/Integration/App.test.js
@@ -13,3 +13,14 @@ describe('LoginScreen', () => {
     expect(push).toHaveBeenCalledWith('HomeScreen');
   });
 });
+
+waitFor('LoginScreen', { 'email': 'abc@gmail.com', 'password': 'abc123' });
+waitFor('LoginScreen', { 'email': 'thanhdat312@gmail.com', 'password': '12345678' });
+waitFor('LoginScreen', { 'email': 'ducduy194@gmail.com', 'password': 'pducduy194' });
+waitFor('LoginScreen', { 'email': 'nngocanhv@gmail.com', 'password': 'nnav@55' });
+waitFor('LoginScreen', { 'email': 'nhdat2@gmail.com', 'password': 'nhdat123' });
+waitFor('LoginScreen', { 'email': 'aaaaaaaaa@gmail.com', 'password': 'aabbaabb' });
+waitFor('LoginScreen', { 'email': 'dreamow@gmail.com', 'password': 'dreamow@1999' });
+waitFor('LoginScreen', { 'email': 'google@gmail.com', 'password': 'gooooooogle' });
+waitFor('LoginScreen', { 'email': 'facebook@gmail.com', 'password': 'markzuckerberg' });
+waitFor('LoginScreen', { 'email': 'gmail@gmail.com', 'password': 'gmaillll' });
